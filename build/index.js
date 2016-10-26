f7f33d98b6c3df3655d5c4ead8af0b319d8fda98
gokhanayhan38 <gokhanayhan38@outlook.com>
2016-10-26 10:21:29 +0300

    inprogress

--- build/index.js
+++ build/index.js
@@ -1,5 +1,26 @@
 "use strict";
 
+const Map = require("./src/map");
+const TileLayer = require("./src/tile-layer");
+const CanvasTileLayer = require("./src/canvas-tile-layer");
+const GridLayer = require("./src/grid-layer");
+const HeatLayer = require("./src/heat-layer");
+const MarkerClusterGroupLayer = require("./src/marker-cluster-group-layer");
+const ChoroplethLayerContainer = require("./src/choropleth-layer-container");
+const EditControl = require("./src/edit-control");
+
+module.exports = {
+    Map: Map,
+    TileLayer: TileLayer,
+    CanvasTileLayer: CanvasTileLayer,
+    GridLayer: GridLayer,
+    HeatLayer: HeatLayer,
+    MarkerClusterGroupLayer: MarkerClusterGroupLayer,
+    ChoroplethLayerContainer: ChoroplethLayerContainer,
+    EditControl: EditControl
+};
+"use strict";
+
 const { PropTypes } = require("react");
 const RL_CanvasTileLayer = require("react-leaflet").CanvasTileLayer;
 const _ = require("underscore");
